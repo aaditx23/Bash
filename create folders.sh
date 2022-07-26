a887a49fbfcbeee663495ca1e209af834d803d65
Md. Amir Ul Islam <97362178+aaditx23@users.noreply.github.com>
2022-07-26 18:39:35 +0600

    First upload
    Creates folder
    Enters created folder
    Create text file
    Populate text file
    Change text file name
    Add more texts in the file containing the new name

---
+++ create folders.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+
+sudo mkdir /mnt/Disk_C
+echo "Created Disk_C folder"
+sudo mkdir /mnt/Disk_D
+echo "Created Disk_D folder"
+sudo mkdir /mnt/Disk_E
+echo "Created Disk_E folder"
+sudo mkdir /mnt/Dev_Edu
+echo "Created Dev_Edu folder"
+sudo mkdir /mnt/Disk_E_HDD2
+echo "Created Disk_E_HDD2 folder"
