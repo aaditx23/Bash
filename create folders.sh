6201c0ee4afbf1cd73a7af003bf76228db95f50b
Md. Amir Ul Islam <97362178+aaditx23@users.noreply.github.com>
2022-07-26 19:03:09 +0600

    Update create folders.sh

--- create folders.sh
+++ create folders.sh
@@ -1,12 +1,25 @@
 #!/bash
 
 mkdir newfolder
+echo "newfolder Created"
 cd newfolder
+echo "Entered newfolder"
 touch newtextfile.txt
+echo "Created newtextfile.txt"
 echo "This is a new text file. " >> newtextfile.txt
 echo "Name of text file: newtextfile.txt" >> newtextfile.txt
+echo "Added lines to newtextfile.txt"
 read -p "Enter new name for file: " newname
+echo "New name of file accepted"
 mv newtextfile.txt $newname.txt
-echo "New name of file is: $newname.txt"
+echo "Renamed file as $newname.txt"
+echo "New name of file is: $newname.txt" >> $newname.txt
+echo "New line added with new name of file"
+new_var=5
+echo "New variable with name new_var created"
+echo "new_var = $new_var" >> $newname.txt
+echo "Line added with the value of new_var"
+
+
 
 
